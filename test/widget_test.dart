29e6c7a23862eb586c24ad1deb0f9f2f52c9f806
DavidDSaputra <davidsaputra2704@gmail.com>
2026-06-21 00:00:05 +0700

    Integrate module file: test/widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,14 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:globalwallet/main.dart';
+import 'package:dompet_kampus_global/core/constants/app_constants.dart';
+import 'package:dompet_kampus_global/core/utils/currency_formatter.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('uses Dompet Kampus Global app constants', () {
+    expect(AppConstants.appName, 'Dompet Kampus Global');
+    expect(AppConstants.apiVersion, '/v1');
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('formats Indonesian Rupiah values', () {
+    expect(CurrencyFormatter.format(250000), 'Rp250.000');
   });
 }
